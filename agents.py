7e95f9920d89062ebf29af75b2d7e05b9c16f5a0
Anshul Malviya <anshulmalviya1725@gmail.com>
2026-09-13 23:50:12 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -15,7 +15,7 @@ load_dotenv()
 groq_api_key = os.getenv("GROQ_API_KEY")
 
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+    model="openai/gpt-oss-120b",
     temperature=0,
     api_key=groq_api_key,
 )
